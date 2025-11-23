8147338e3d7605e233e07941d35dc5f7a43b7b35
dl2gomi <dl2gomi@gmail.com>
2025-11-23 16:19:10 -0600

    be-hotfix: render env issue fixed

--- backend/config.ts
+++ backend/config.ts
@@ -1,17 +1,12 @@
 /* eslint-disable n/no-process-env */
-
-import path from 'path';
 import dotenv from 'dotenv';
 import moduleAlias from 'module-alias';
 
-
 // Check the env
-const NODE_ENV = (process.env.NODE_ENV ?? 'development');
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
 
 // Configure "dotenv"
-const result2 = dotenv.config({
-  path: path.join(__dirname, `./config/.env.${NODE_ENV}`),
-});
+const result2 = dotenv.config();
 if (result2.error) {
   throw result2.error;
 }
